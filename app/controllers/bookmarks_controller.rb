c619818c607fe43ccdb937c7da4faeff241933fd
Benjamin Moreaux <benjy567@gmail.com>
2024-03-08 16:44:22 +0100

    cloudinary mis en place

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,27 +1,30 @@
 class BookmarksController < ApplicationController
 
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(params[:list_id])
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params(:id))
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_bookmark_path(@bookmark)
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie, :list)
+    params.require(:bookmark).permit(:comment, :movie_id, :list)
   end
 end
